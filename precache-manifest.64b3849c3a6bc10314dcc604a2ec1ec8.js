da8c2aaec766a760e673599ba98a770dd5257d8d
rosario VS <paradisekiss-yaoi@hotmail.com>
2019-11-07 11:02:39 -0600

    Updates

---
+++ precache-manifest.64b3849c3a6bc10314dcc604a2ec1ec8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "038fa5c6797d970134749086156013d4",
+    "url": "/happy-or-grumpty-tictactoe/index.html"
+  },
+  {
+    "revision": "b446669e0dccc1553781",
+    "url": "/happy-or-grumpty-tictactoe/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "61aa2bfdbd8bb2dd6727",
+    "url": "/happy-or-grumpty-tictactoe/static/js/2.47696e14.chunk.js"
+  },
+  {
+    "revision": "b446669e0dccc1553781",
+    "url": "/happy-or-grumpty-tictactoe/static/js/main.bf114270.chunk.js"
+  },
+  {
+    "revision": "d2c8329c2eb82584f4c4",
+    "url": "/happy-or-grumpty-tictactoe/static/js/runtime-main.f8052a83.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/happy-or-grumpty-tictactoe/static/media/logo.25bf045c.svg"
+  }
+]);
