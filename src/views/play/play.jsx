f641d707e5ca59a248e7769b189e2ea6e4f7004a
rosario VS <paradisekiss-yaoi@hotmail.com>
2019-11-11 00:30:27 -0600

    Creando componentes del juego y cambiando estilos de tablero, seguí documentación de React y luego usé hooks

---
+++ src/views/play/play.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Game from "../../components/gameComponents/game/game";
+
+function Play() {
+  return (
+    <div className="play-view">
+      <Game/>
+    </div>
+  );
+}
+
+export default Play
+
